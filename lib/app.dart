9bf0fbaaac5538e33ae36e2c07c4f380101a6b12
onishi taisei <taisei.developer@gmail.com>
2023-12-05 02:09:25 +0900

    🚜 refactor: ディレクトリ構成変更、不要なコメントの削除

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:advent_calendar_2023/ui/counter.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Golden Test',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Golden Test Demo'),
+    );
+  }
+}
